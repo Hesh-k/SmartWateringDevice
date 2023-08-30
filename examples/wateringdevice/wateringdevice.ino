d3477f1882e735b615ad8d6fb16000e7cf4697fd
IT22589804 <it22589804@my.sliit.lk>
2023-08-30 20:02:45 +0530

    Updated

--- examples/wateringdevice/wateringdevice.ino
+++ examples/wateringdevice/wateringdevice.ino
@@ -1,50 +1,29 @@
-﻿#define BLYNK_TEMPLATE_ID           "TMPLxxxxxx"
-#define BLYNK_DEVICE_NAME           "Device"
-#define BLYNK_AUTH_TOKEN            "YourAuthToken"
+#define BLYNK_TEMPLATE_ID           "TMPL6tijVHgxM"
+#define BLYNK_DEVICE_NAME           "Quickstart Template"
+#define BLYNK_AUTH_TOKEN            "EzJlsm5nK64IDDoA0MZ6RxpltK9Fuxzh"
 
 #define BLYNK_PRINT Serial
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include <DNSServer.h>
+#include <ESP8266WebServer.h>
 #include <WiFiManager.h>
-#include <Button2.h>
-#include <Ticker.h>
 
 char auth[] = BLYNK_AUTH_TOKEN;
-char ssid[40];
-char pass[40];
+int timeout = 120; // seconds to run the configuration portal
+int triggerPin = 0; // Pin to trigger the configuration portal
 
-Button2 physicalButton = Button2(0, INPUT_PULLUP);  // Change '0' to the actual button pin
-Ticker timer;
+BlynkTimer timer;
 
-int inPin = D7;
-int modePin = D9;
-int Control = D12;
-
+int inPin = 7;
+int modePin = 9;
+int Control = 12;
 const int AirValue = 4000;
 const int WaterValue = 0;
 int intervals = (AirValue - WaterValue) / 3;
 int soilMoistureValue, humidity, humiditys = 0;
 bool condition = false;
-
-void configModeCallback (WiFiManager *myWiFiManager) {
-  Serial.println("Entered config mode. You can reset settings here.");
-}
-
-void setupWiFiManager() {
-  WiFiManager wm;
-  wm.setAPCallback(configModeCallback);
-  
-  if (!wm.autoConnect("AutoConnectAP")) {
-    Serial.println("Failed to connect and hit timeout. Resetting...");
-    ESP.restart();
-    delay(1000);
-  }
-  
-  strcpy(ssid, wm.getWiFiSSID());
-  strcpy(pass, wm.getWiFiPass());
-}
 
 void myTimerEvent() {
   Blynk.virtualWrite(V6, humiditys);
@@ -78,22 +57,31 @@ void Soilmoisture() {
   delay(100);
 }
 
-void buttonPress() {
-  Serial.println("Button pressed! Resetting WiFi settings...");
-  WiFiManager wm;
-  wm.resetSettings();
-  ESP.restart();
-}
-
 void setup() {
   Serial.begin(115200);
-  
-  setupWiFiManager();
-  
-  physicalButton.setPressedHandler(buttonPress);
-  
-  Blynk.begin(auth, ssid, pass);
-  timer.attach_ms(1000, myTimerEvent);
+  WiFi.mode(WIFI_STA);
+  pinMode(triggerPin, INPUT_PULLUP);
+
+  if (digitalRead(triggerPin) == LOW) {
+    WiFiManager wm;
+    wm.setConfigPortalTimeout(timeout);
+
+    if (!wm.startConfigPortal("OnDemandAP")) {
+      Serial.println("Failed to connect and hit timeout");
+      delay(3000);
+      ESP.restart();
+      delay(5000);
+    }
+
+    Serial.println("Connected to WiFi");
+    Serial.print("Local IP: ");
+    Serial.println(WiFi.localIP());
+  }
+
+  Blynk.config(auth);
+  Blynk.connect();
+
+  timer.setInterval(1000L, myTimerEvent);
   pinMode(inPin, INPUT);
   pinMode(modePin, OUTPUT);
   pinMode(Control, OUTPUT);
@@ -102,8 +90,7 @@ void setup() {
 }
 
 void loop() {
-  physicalButton.loop();
   Blynk.run();
-  timer.update();
+  timer.run();
   Soilmoisture();
 }
